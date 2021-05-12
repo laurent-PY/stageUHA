3e4ea3af42c388c86fa06f5b921de949f2cf1084
laurent.bichet <bichet.laurent@gmail.com>
2021-05-12 02:28:50 +0200

    suppression dossier view activites

--- views/activites/index.php
+++
@@ -1,7 +0,0 @@
-<h1>liste des activités</h1>
-<?php foreach($activites as $activite): ?>
-    <h2><?= $activite['activite'] ?></h2>
-    <p><?= $activite['']?></p>
-//TODO à terminer.
-
-<?php endforeach; ?>
